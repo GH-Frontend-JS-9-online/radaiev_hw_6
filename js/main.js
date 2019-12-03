cab23d86283cdbd3a209ae5bac6a4494eb03d700
vitalii_radaiev <hf.chelenger@gmail.com>
2019-12-03 13:30:12 +0300

    calc

--- js/main.js
+++ js/main.js
@@ -151,5 +151,5 @@ function matchLetters(form) {
 	alert(`Совпадение слов: ${result}%`);
 }
 
-// text += "\nСтрока в верхнем регистре - " + string.toUpperCase();
-// text += "\nСтрока в нижнем регистре - " + string.toLowerCase();
+
+
